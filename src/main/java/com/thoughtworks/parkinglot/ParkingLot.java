457112170d9e7c043ccc28837caa1ba6ea55ae7f
shreyascr <shrs601@gmail.com>
2019-10-03 13:17:16 +0530

    [shreyas|chakri] Add.is space Available should true

---
+++ src/main/java/com/thoughtworks/parkinglot/ParkingLot.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.parkinglot;
+
+public class ParkingLot {
+
+    private int space;
+
+    public ParkingLot(int space) {
+        this.space = space;
+    }
+
+    public boolean isAvailable() {
+        return true;
+    }
+}
---
+++ src/test/java/com/thoughtworks/parkinglot/ParkingLotTest.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.parkinglot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ParkingLotTest
+{
+    @Test
+    void givenParkingLot_WhenIsAvailable_ThenMustPark(){
+        ParkingLot parkingLot=new ParkingLot(10);
+
+        Assertions.assertEquals(true,parkingLot.isAvailable());
+
+    }
+}
