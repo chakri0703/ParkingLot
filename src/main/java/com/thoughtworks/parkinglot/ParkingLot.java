e5d6bb5b3a42f9cb33e6a6bdbb75697cefd68372
chakravarthig <gchakravarthia8@gmail.com>
2019-10-03 23:40:33 +0530

    Add. no vehicle is parked then unable to unpark.

--- src/main/java/com/thoughtworks/parkinglot/ParkingLot.java
+++ src/main/java/com/thoughtworks/parkinglot/ParkingLot.java
@@ -35,4 +35,8 @@ public class ParkingLot {
     protected boolean isParkingLotFull() {
         return vehicles.size() >= capacity;
     }
+
+    public boolean unPark(Object vehicle) {
+        return  false;
+    }
 }
--- src/test/java/com/thoughtworks/parkinglot/ParkingLotTest.java
+++ src/test/java/com/thoughtworks/parkinglot/ParkingLotTest.java
@@ -1,24 +1,25 @@
 package com.thoughtworks.parkinglot;
 
 import com.thoughtworks.parkinglot.exception.ParkingLotFullException;
+import com.thoughtworks.parkinglot.exception.VehicleAlreadyParkedException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingLotTest {
     @Test
-    void givenParkingLot_WhenPark_ThenMustPark() throws ParkingLotFullException {
+    void givenParkingLot_WhenPark_ThenMustPark() throws ParkingLotFullException, VehicleAlreadyParkedException {
         ParkingLot parkingLot = new ParkingLot(1);
 
-        Object vehicel =new Object();
-        parkingLot.park(vehicel);
+        Object vehicle =new Object();
+        parkingLot.park(vehicle);
 
         assertTrue(parkingLot.isParkingLotFull());
 
     }
 
     @Test
-    void givenParkingLotIsFull_WhenPark_ThenMustNotPark() throws ParkingLotFullException {
+    void givenParkingLotIsFull_WhenPark_ThenMustNotPark() throws ParkingLotFullException, VehicleAlreadyParkedException {
         ParkingLot parkingLot = new ParkingLot(1);
 
         parkingLot.park(new Object());
@@ -28,15 +29,23 @@ class ParkingLotTest {
     }
 
     @Test
-    void givenParkingLotWithSameCars_WhenPark_ThenMustNotPark() throws ParkingLotFullException {
+    void givenParkingLotWithSameCars_WhenPark_ThenMustNotPark() throws ParkingLotFullException, VehicleAlreadyParkedException {
         ParkingLot parkingLot = new ParkingLot(2);
 
         Object object = new Object();
 
         parkingLot.park(object);
 
-        Throwable exception = assertThrows(ParkingLotFullException.class, () -> parkingLot.park(object));
+        Throwable exception = assertThrows(VehicleAlreadyParkedException.class, () -> parkingLot.park(object));
         assertEquals(exception.getMessage(), "vehicle already parked");
+    }
+
+    @Test
+    void givenParkedOneCar_WhenUnPark_ThenTheyShouldBeAbleToUnPark(){
+        ParkingLot parkingLot=new ParkingLot(1);
+
+        Object vehicle=new Object();
+        assertEquals(false,parkingLot.unPark(vehicle));
     }
 
 
