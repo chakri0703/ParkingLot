d84ce269bf1844b0f8ecc6ef2cf16e7d3796b0bb
chakravarthig <gchakravarthia8@gmail.com>
2019-10-03 23:26:52 +0530

    add. exception handling in consumer side

--- src/main/java/com/thoughtworks/parkinglot/consumer/Sanjay.java
+++ src/main/java/com/thoughtworks/parkinglot/consumer/Sanjay.java
@@ -1,28 +1,4 @@
 package com.thoughtworks.parkinglot.consumer;
 
-import com.thoughtworks.parkinglot.ParkingLot;
-import com.thoughtworks.parkinglot.exception.ParkingLotFullException;
-
 public class Sanjay {
-
-    public static void park(ParkingLot one, ParkingLot two) throws ParkingLotFullException {
-
-        Object firsCar = new Object();
-
-        try {
-            one.park(firsCar);
-            one.park(firsCar);
-        } catch (ParkingLotFullException parkinglotExceptions) {
-            System.out.println(parkinglotExceptions.getMessage());
-        }
-
-    }
-
-    public static void main(String[] args) throws ParkingLotFullException {
-        ParkingLot one = new ParkingLot(2);
-        ParkingLot two = new ParkingLot(3);
-
-        Sanjay.park(one, two);
-        Sanjay2.park(one,two);
-    }
 }
